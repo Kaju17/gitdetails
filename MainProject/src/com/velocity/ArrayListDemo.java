f29cc10a966f132398035bf59a60d7d0de51219e
kajal narwade <kajalnarwade17@gmail.com>
2023-01-09 17:36:42 +0530

    First commit

---
+++ MainProject/src/com/velocity/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+	public static void main(String[] args) {
+		List<String> list=new ArrayList<String>();
+		list.add("kajal");
+		list.add("shraddha");
+		list.add("saurabh");
+		list.add("param");
+		list.add("lokesh");
+		System.out.println(list);
+	}
+
+}
